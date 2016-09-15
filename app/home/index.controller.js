fc15a48ed018c1fb59a54e991b65800e30b3d1b5
Ricardo Gamboa <ricardo.gamboa@live.com>
2016-09-15 18:29:06 -0300

    Login and villagers control

---
+++ app/home/index.controller.js
@@ -0,0 +1,378 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('app')
+        .controller('Home.IndexController', Controller);
+
+    function Controller(UserService, $scope, $interval, FlashService) {
+        var vm = this;
+
+        vm.user = null;
+        vm.saveUser = saveUser;
+
+        initController();
+
+        function initController() {
+            // get current user
+            UserService.GetCurrent().then(function (user) {
+                vm.user = user;
+            });
+        }
+        function saveUser() {
+            UserService.Update(vm.user)
+                .then(function () {
+                    FlashService.Success('User updated');
+                })
+                .catch(function (error) {
+                    FlashService.Error(error);
+                });
+        }
+        $scope.spendResources = function(food, wood, stone, ore, quantity) {
+            if ($scope.vm.user.food >= food*quantity && $scope.vm.user.wood >= wood*quantity && $scope.vm.user.stone >= stone*quantity && $scope.vm.user.ore >= ore*quantity){
+                $scope.vm.user.food = $scope.vm.user.food- food*quantity;
+                $scope.vm.user.wood = $scope.vm.user.wood- wood*quantity;
+                $scope.vm.user.stone = $scope.vm.user.stone - stone*quantity;
+                $scope.vm.user.ore = $scope.vm.user.ore - ore*quantity;
+                return true;
+            }
+            else return false;
+            }
+        $scope.farmers = function(add,dead) { //Con add igual a cantidad de aldeanos que se agregan a los granjeros
+            if ($scope.vm.user.villagers >= add && add > 0 && $scope.spendResources(100,50,0,0,add)) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                $scope.vm.user.farmers = $scope.vm.user.farmers + add;
+            }
+            if ($scope.vm.user.farmers >= -add && add < 0) {
+                if (dead==1){
+                    $scope.vm.user.farmers = $scope.vm.user.farmers + add;
+                }
+                else {
+                    $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                    $scope.vm.user.farmers = $scope.vm.user.farmers + add;
+                }
+            }
+        }
+        $scope.hunters = function(add,dead) { //Con add igual a cantidad de aldeanos que se agregan a los granjeros
+            if ($scope.vm.user.villagers >= add && add > 0 && $scope.spendResources(0,80,20,10,add)) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                $scope.vm.user.hunters = $scope.vm.user.hunters + add;
+            }
+            if ($scope.vm.user.hunters >= -add && add < 0) {
+                if (dead==1){
+                    $scope.vm.user.hunters = $scope.vm.user.hunters + add;
+                }
+                else {
+                    $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                    $scope.vm.user.hunters = $scope.vm.user.hunters + add;
+                }
+            }
+        }
+        $scope.ranchers = function(add,dead) { //Con add igual a cantidad de aldeanos que se agregan a los granjeros
+            if ($scope.vm.user.villagers >= add && add > 0 && $scope.spendResources(80,80,0,0,add)) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                $scope.vm.user.ranchers = $scope.vm.user.ranchers + add;
+            }
+            if ($scope.vm.user.ranchers >= -add && add < 0) {
+                if (dead==1){
+                    $scope.vm.user.ranchers = $scope.vm.user.ranchers + add;
+                }
+                else {
+                    $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                    $scope.vm.user.ranchers = $scope.vm.user.ranchers + add;
+                }
+            }
+        }
+        $scope.gatherers = function(add,dead) { //Con add igual a cantidad de aldeanos que se agregan a los granjeros
+            if ($scope.vm.user.villagers >= add && add > 0) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                $scope.vm.user.gatherers = $scope.vm.user.gatherers + add;
+            }
+            if ($scope.vm.user.gatherers >= -add && add < 0) {
+                if (dead==1){
+                    $scope.vm.user.gatherers = $scope.vm.user.gatherers + add;
+                }
+                else {
+                    $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                    $scope.vm.user.gatherers = $scope.vm.user.gatherers + add;
+                }
+            }
+        }
+        $scope.lumberjacks = function(add,dead) { //Con add igual a cantidad de aldeanos que se agregan a los leñadores
+            if ($scope.vm.user.villagers >= add && add > 0) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                $scope.vm.user.lumberjacks = $scope.vm.user.lumberjacks + add;
+            }
+            if ($scope.vm.user.lumberjacks >= -add && add < 0) {
+                if (dead==1){
+                    $scope.vm.user.lumberjacks = $scope.vm.user.lumberjacks + add;
+                }
+                else {
+                    $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                    $scope.vm.user.lumberjacks = $scope.vm.user.lumberjacks + add;
+                }
+            }
+        }
+        $scope.stoneMiners = function(add,dead) {
+            if ($scope.vm.user.villagers >= add && add > 0) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                $scope.vm.user.stoneMiners = $scope.vm.user.stoneMiners + add;
+            }
+            if ($scope.vm.user.stoneMiners >= -add && add < 0) {
+                if (dead==1){
+                    $scope.vm.user.stoneMiners = $scope.vm.user.stoneMiners + add;
+                }
+                else {
+                    $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                    $scope.vm.user.stoneMiners = $scope.vm.user.stoneMiners + add;
+                }
+            }
+        }
+        $scope.oreMiners = function(add,dead) {
+            if ($scope.vm.user.villagers >= add && add > 0) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                $scope.vm.user.oreMiners = $scope.vm.user.oreMiners + add;
+            }
+            if ($scope.vm.user.oreMiners >= -add && add < 0) {
+                if (dead==1){
+                    $scope.vm.user.oreMiners = $scope.vm.user.oreMiners + add;
+                }
+                else {
+                    $scope.vm.user.villagers = $scope.vm.user.villagers - add;
+                    $scope.vm.user.oreMiners = $scope.vm.user.oreMiners + add;
+                }
+            }
+        }
+        $scope.villagers = function(add) {
+            if (add > 0 && $scope.spendResources($scope.villagerCost(),0,0,0,add)) {
+                $scope.vm.user.villagers = $scope.vm.user.villagers + add;
+            }
+            if (add < 0) {
+                if ($scope.vm.user.villagers >= -add) $scope.vm.user.villagers =$scope.vm.user.villagers +add;
+                else $scope.vm.user.villagers = 0;
+            }
+        }
+        $scope.allVillagers = function() {
+            return ($scope.vm.user.villagers + $scope.vm.user.farmers + $scope.vm.user.lumberjacks + $scope.vm.user.stoneMiners + $scope.vm.user.oreMiners + $scope.vm.user.gatherers + $scope.vm.user.hunters + $scope.vm.user.ranchers);
+        }
+        $scope.villagerCost = function(){
+            return Math.round(10+10*Math.pow($scope.allVillagers() , 1.4));
+        }
+        $scope.food = function() {
+            if ($scope.vm.user.food < 500)
+                return $scope.vm.user.food = $scope.vm.user.food + 1;
+            if ($scope.vm.user.windmill==true)
+                $scope.vm.user.food = $scope.vm.user.food + 1;
+        }
+        $scope.wood = function() {
+            if ($scope.vm.user.sawmill==1)
+                return $scope.vm.user.wood = $scope.vm.user.wood + 1;
+            if ($scope.vm.user.wood < 500 )
+                $scope.vm.user.wood = $scope.vm.user.wood + 1;
+        }
+        $scope.stone = function() {
+            if ($scope.vm.user.quarry==1)
+                return $scope.vm.user.stone = $scope.vm.user.stone + 1;
+            if ($scope.vm.user.stone < 500 )
+                $scope.vm.user.stone = $scope.vm.user.stone + 1;
+        }
+        $scope.ore = function() {
+            if ($scope.vm.user.mine==1)
+                return $scope.vm.user.ore = $scope.vm.user.ore + 1;
+            if ($scope.vm.user.ore < 500 )
+                $scope.vm.user.ore = $scope.vm.user.ore + 1;
+        }
+        $scope.deadvillager = function(number) {
+            if ($scope.vm.user.villagers >= -number){
+              $scope.villagers(number);  
+            } else if ($scope.vm.user.villagers + $scope.vm.user.lumberjacks > -number){
+                $scope.lumberjacks(number + $scope.vm.user.villagers,1);
+                $scope.villagers(- $scope.vm.user.villagers);
+            } else if ($scope.vm.user.villagers + $scope.vm.user.lumberjacks + $scope.vm.user.stoneMiners >= -number){
+                $scope.stoneMiners(number + $scope.vm.user.villagers + $scope.vm.user.lumberjacks,1);
+                $scope.lumberjacks(- $scope.vm.user.lumberjacks,1);
+                $scope.villagers(- $scope.vm.user.villagers);
+            } else if ($scope.vm.user.villagers + $scope.vm.user.lumberjacks + $scope.vm.user.stoneMiners + $scope.vm.user.oreMiners >= -number){
+                $scope.oreMiners(number + $scope.vm.user.villagers + $scope.vm.user.lumberjacks + $scope.vm.user.stoneMiners,1);
+                $scope.lumberjacks(- $scope.vm.user.lumberjacks,1);
+                $scope.stoneMiners(- $scope.vm.user.stoneMiners,1);
+                $scope.villagers(- $scope.vm.user.villagers);
+            } else {
+                $scope.oreMiners(- $scope.vm.user.oreMiners,1);
+                $scope.lumberjacks(- $scope.vm.user.lumberjacks,1);
+                $scope.stoneMiners(- $scope.vm.user.stoneMiners,1);
+                $scope.villagers(- $scope.vm.user.villagers);
+            }
+        }
+
+        //Funciones de tecnologia
+        $scope.agriculture = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.agriculture=1;
+           }
+        }
+        $scope.animalHusbandry = function() {
+            if ($scope.spendResources(100,200,50,0,1)) {
+                $scope.vm.user.animalHusbandry=1;
+           }
+        }
+        $scope.trapping = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.trapping=1;
+           }
+        }
+        $scope.mining = function() {
+            if ($scope.spendResources(0,100,0,0,1)) {
+                $scope.vm.user.mining=1;
+           }
+        }
+        $scope.masonry = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.masonry=1;
+           }
+        }
+        $scope.construction = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.construction=1;
+           }
+        }
+        $scope.iron = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.iron=1;
+           }
+        }
+        $scope.tool = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.tool=1;
+           }
+        }
+        $scope.wheel = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.wheel=1;
+           }
+        }
+        $scope.road = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.road=1;
+           }
+        }
+        $scope.currency = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.currency=1;
+           }
+        }
+        $scope.guild = function() {    
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.guild=1;
+           }
+        }
+        $scope.cart = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.cart=1;
+           }
+        }
+
+        //funciones de contruccion
+        $scope.windmill = function() {
+            if ($scope.spendResources(0,500,200,0,1)) {
+                $scope.vm.user.windmill=1;
+           }
+        }
+        $scope.sawmill = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.sawmill=1;
+           }
+        }
+        $scope.furnace = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.furnace=1;
+           }
+        }
+        $scope.rockRoad = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.rockRoad=1;
+           }
+        }
+        $scope.market = function() {    
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.market=1;
+           }
+        }
+        $scope.blacksmith = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.blacksmith=1;
+           }
+        }
+        $scope.hut = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.hut=1;
+           }
+        }
+        $scope.cabin = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.cabin=1;
+           }
+        }
+        $scope.house = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.house=1;
+           }
+        }
+        $scope.mine = function() {
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.mine=1;
+           }
+        }
+        $scope.quarry = function() {    
+            if ($scope.spendResources(200,100,0,0,1)) {
+                $scope.vm.user.quarry=1;
+           }
+        } 
+        $scope.foodChange=0;
+        $scope.woodChange=0;
+        $scope.stoneChange=0;
+        $scope.oreChange=0;
+        $scope.editing=0;
+        var nofood=0;
+        $interval(function() {
+            $scope.foodChange= $scope.vm.user.gatherers + $scope.vm.user.farmers*(3/2) + $scope.vm.user.hunters*(3/2) - (($scope.allVillagers() - ($scope.vm.user.villagers* (1 / 2))) * (1 / 2));
+            nofood= $scope.vm.user.food +  $scope.foodChange;
+            if (nofood < 0){
+                $scope.vm.user.food = 0;
+                $scope.deadvillager(Math.round(nofood/5));
+            } else $scope.vm.user.food = nofood;
+            $scope.woodChange = $scope.vm.user.lumberjacks/ 2;
+            $scope.stoneChange = $scope.vm.user.stoneMiners / 3;
+            $scope.oreChange = $scope.vm.user.oreMiners / 10;
+            $scope.vm.user.wood = $scope.vm.user.wood + $scope.woodChange;
+            $scope.vm.user.stone = $scope.vm.user.stone + $scope.stoneChange;
+            $scope.vm.user.ore = $scope.vm.user.ore + $scope.oreChange;
+            if ($scope.vm.user.food > 500){
+                if ($scope.vm.user.windmill!=1){
+                    $scope.vm.user.food = 500;
+                }
+            }
+            if ($scope.vm.user.wood > 500){
+                if ($scope.vm.user.sawmill!=1){
+                    $scope.vm.user.wood = 500;
+                }
+            }
+            if ($scope.vm.user.stone > 500){
+                if ($scope.vm.user.quarry!=1){
+                    $scope.vm.user.stone = 500;
+                }
+            }
+            if ($scope.vm.user.ore > 500){
+                if ($scope.vm.user.mine!=1){
+                    $scope.vm.user.ore = 500;
+                }
+            }
+
+        }, 1000);
+        $interval(function() {
+            vm.saveUser();
+        }, 20000);
+    };
+
+    }
+
+)();
---
+++ services/user.service.js
@@ -0,0 +1,230 @@
+﻿var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('users');
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getById = getById;
+service.create = create;
+service.update = update;
+service.delete = _delete;
+
+module.exports = service;
+
+function authenticate(username, password) {
+    var deferred = Q.defer();
+
+    db.users.findOne({ username: username }, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user && bcrypt.compareSync(password, user.hash)) {
+            // authentication successful
+            deferred.resolve(jwt.sign({ sub: user._id }, config.secret));
+        } else {
+            // authentication failed
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getById(_id) {
+    var deferred = Q.defer();
+
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user) {
+            // return user (without hashed password)
+            deferred.resolve(_.omit(user, 'hash'));
+        } else {
+            // user not found
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function create(userParam) {
+    var deferred = Q.defer();
+
+
+    // validation
+    db.users.findOne(
+        { username: userParam.username },
+        function (err, user) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            if (user) {
+                // username already exists
+                deferred.reject('Username "' + userParam.username + '" is already taken');
+            } else {
+                createUser();
+            }
+        });
+
+    function createUser() {
+        // set user object to userParam without the cleartext password
+        userParam.villagers=0;
+        userParam.gatherers=0;
+        userParam.lumberjacks=0;
+        userParam.stoneMiners=0;
+        userParam.metalMiners=0;
+        userParam.oreMiners=0;
+        userParam.farmers=0;
+        userParam.hunters=0;
+        userParam.ranchers=0;
+        userParam.food=0;
+        userParam.wood=0;
+        userParam.stone=0;
+        userParam.ore=0;
+        userParam.metal=0;
+        userParam.agriculture=0;
+        userParam.animalHusbandry=0;
+        userParam.trapping=0;
+        userParam.mining=0;
+        userParam.masonry=0;
+        userParam.construction=0;
+        userParam.iron=0;
+        userParam.tool=0;
+        userParam.wheel=0;
+        userParam.road=0;
+        userParam.currency=0;
+        userParam.guild=0;
+        userParam.cart=0;
+        userParam.windmill=0;
+        userParam.sawmill=0;
+        userParam.furnace=0;
+        userParam.rockRoad=0;
+        userParam.market=0;
+        userParam.blacksmith=0;
+        userParam.hut=0;
+        userParam.cabin=0;
+        userParam.house=0;
+        userParam.mine=0;
+        userParam.quarry=0;
+        var user = _.omit(userParam, 'password');
+
+        // add hashed password to user object
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+
+        db.users.insert(
+            user,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function update(_id, userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user.username !== userParam.username) {
+            // username has changed so check if the new username is already taken
+            db.users.findOne(
+                { username: userParam.username },
+                function (err, user) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+
+                    if (user) {
+                        // username already exists
+                        deferred.reject('Username "' + req.body.username + '" is already taken')
+                    } else {
+                        updateUser();
+                    }
+                });
+        } else {
+            updateUser();
+        }
+    });
+
+    function updateUser() {
+        // fields to update
+        var set = {
+            username: userParam.username,
+            villageName: userParam.name,
+            villagers: userParam.villagers,
+            gatherers: userParam.gatherers,
+            lumberjacks: userParam.lumberjacks,
+            stoneMiners: userParam.stoneMiners,
+            metalMiners: userParam.metalMiners,
+            farmers: userParam.farmers,
+            hunters: userParam.hunters,
+            ranchers: userParam.ranchers,
+            food: userParam.food,
+            wood: userParam.wood,
+            stone: userParam.stone,
+            metal: userParam.metal,
+            agriculture: userParam.agriculture,
+            animalHusbandry: userParam.animalHusbandry,
+            trapping: userParam.trapping,
+            mining: userParam.mining,
+            masonry: userParam.masonry,
+            construction: userParam.construction,
+            iron: userParam.iron,
+            tool: userParam.tool,
+            wheel: userParam.wheel,
+            road: userParam.road,
+            currency: userParam.currency,
+            guild: userParam.guild,
+            cart: userParam.cart,
+            windmill: userParam.windmill,
+            sawmill: userParam.sawmill,
+            furnace: userParam.furnace,
+            rockRoad: userParam.rockRoad,
+            market: userParam.market,
+            blacksmith: userParam.blacksmith,
+            hut: userParam.hut,
+            cabin: userParam.cabin,
+            house: userParam.house,
+            mine: userParam.mine,
+            quarry: userParam.quarry
+        };
+
+        // update password if it was entered
+        if (userParam.password) {
+            set.hash = bcrypt.hashSync(userParam.password, 10);
+        }
+
+        db.users.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(_id) {
+    var deferred = Q.defer();
+
+    db.users.remove(
+        { _id: mongo.helper.toObjectID(_id) },
+        function (err) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
